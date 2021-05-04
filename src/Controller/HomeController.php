11bc2e6cda4b47fe67ec99ac74116c7ffcc7d185
Ulises502 <43120036+Ulises502@users.noreply.github.com>
2021-05-04 12:21:11 -0300

    Created a controller and entity + migration to DB

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,12 +7,16 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 
+
+/**
+    * @Route ("/home", name="home")
+    */
 Class HomeController extends AbstractController {
 
     /**
-    * @Route ("/home", name="home")
+    * @Route ("/inicio", name="inicio")
     */
-    public function Home(Request $request) {
+    public function Inicio(Request $request) {
         return $this->render('base.html.twig');
     }
-};    
+};
