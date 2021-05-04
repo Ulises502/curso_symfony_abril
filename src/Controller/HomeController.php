43beca4baa4ab5cffcab2fdce094ee5f976f51da
Ulises502 <43120036+Ulises502@users.noreply.github.com>
2021-05-04 14:33:13 -0300

    Added index.html.twig

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,7 +6,9 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
-
+use Doctrine\ORM\EntityManagerInterface;
+// llama a la entity User
+use App\Entity\User;
 
 /**
     * @Route ("/home", name="home")
@@ -17,6 +19,6 @@ Class HomeController extends AbstractController {
     * @Route ("/inicio", name="inicio")
     */
     public function Inicio(Request $request) {
-        return $this->render('base.html.twig');
+        return $this->render('index.html.twig');
     }
 };
