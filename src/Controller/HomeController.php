ba13afd1b1f901cbb4ee75367729d681ee0097b2
Ulises502 <43120036+Ulises502@users.noreply.github.com>
2021-05-05 14:51:28 -0300

    Added form and login

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,9 +7,10 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 // llama a la entity User
 use App\Entity\User;
-use Symfony\Component\HttpFoundation\RedirectResponse;
+
 
 /**
     * @Route ("/home", name="home")
@@ -17,23 +18,40 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 Class HomeController extends AbstractController {
 
     /**
+    * @Route ("/login", name="login")
+    */
+    public function Login(Request $request) {
+        return $this->render('login.html.twig');
+    }
+
+    /**
     * @Route ("/inicio", name="inicio")
     */
     public function Inicio(Request $request) {
-        return $this->render('index.html.twig');
+        return $this->render('inicio.html.twig');
     }
 
     /**
     * @Route ("/control", name="control")
     */
-    public function Control(Request $request): RedirectResponse {
-        return $this->redirectToRoute('Inicio');
+    public function Control(Request $request) {
+        // extrae los valores que se mandaron en el formulario dentro del $request
+        $nombre = $request->request->get("user");
+        $contrasena = $request->request->get("pass");
+
+        // inicia elemento entidad y usando el Repository busca en la base de datos si hay coincidencia
+        $en = $this-> getDoctrine()-> getManager();
+        $usuario = $en -> getRepository(User::class) -> findOneBy(["name" => $nombre, "password" => $contrasena]);
+
+        // si no hay usuario con los datos del formulario, va a error; si coincide uno, va a inicio
+        if ($usuario == null) {
+            return $this->render('error-login.html.twig');
+        }
+        else {
+            // redirects to the "inicio" route
+            return $this->redirect('./inicio');
+        }
+        
     }
 
-    /**
-    * @Route ("/exito", name="exito")
-    */
-    public function Exito(Request $request) {
-        return $this->render('exito.html.twig');
-    }
 };
